4449171d94f4c49ea31db7a38c234cf408f4e309
Luke Cho <luke.h.cho@gmail.com>
2021-09-18 16:18:56 -0400

    Backend, config the router / server connections, currently working on controllers

---
+++ server/controllers/userController.js
@@ -0,0 +1,40 @@
+const { WSAETIMEDOUT } = require('constants');
+const User = require('../models/userModel');
+//empty obj to store middlewares
+const userController = {};
+
+userController.getItems = (req, res, next) => {
+  console.log(req.body)
+  //req.body will only have username, so just put it in as an parameter
+  User.findOne(req.body)
+    .then(users => {
+      res.locals.users = users;
+      next();
+    })
+    .catch(err => next(err));
+};
+
+userController.deleteItem = (req, res, next) => {
+
+}
+
+userController.editItems = (req, res, next) => {
+  
+  const username = {username: req.body.username};
+  const editItems = {}
+  User.findOneAndUpdate(username, {$set: req.body}, {new:true})
+}
+
+// userController.addUser = (req, res, next) => {
+//   console.log(req.body)
+//   User.create(req.body)
+//   .then(users => {
+//     res.locals.users = users;
+//     console.log(users)
+//     next();
+//   })
+//   .catch(err => next(err));
+// }
+
+
+module.exports = userController;
