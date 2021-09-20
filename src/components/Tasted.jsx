0b7581a54b3c2c1a5387756c633c7e0375d97e4a
crossjp1 <Jamespvcross@gmail.com>
2021-09-20 16:39:49 -0400

    Added Date Logic

--- src/components/Tasted.jsx
+++ src/components/Tasted.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import TastedCard from './TastedCard.jsx';
 
-function Tasted({ tasted, moveTastedToFridge }) {
+function Tasted({ tasted, moveTastedToFridge, getDifferenceInDays }) {
 	const tastedCards = tasted.map((food) => {
-		return <TastedCard food={food} moveTastedToFridge={moveTastedToFridge} />;
+		return (
+		<TastedCard 
+			food={food} 
+			moveTastedToFridge={moveTastedToFridge} 
+			getDifferenceInDays={getDifferenceInDays}
+		/>
+	);
 	});
 
 	return <div>{tastedCards}</div>;
